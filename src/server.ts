e47d4b01d1115120818cdbe48d2af91d169f0e7d
clambert2385 <planeteer85@gmail.com>
2021-04-12 18:17:03 -0700

    Adding my first push of work

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,34 @@
+require('dotenv').config();
 import express from 'express';
+import { sequelize } from './sequelize';
+
+import { IndexRouter } from './controllers/v0/index.router';
+
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+import { V0MODELS } from './controllers/v0/model.index';
 
 (async () => {
+  await sequelize.addModels(V0MODELS);
+  await sequelize.sync();
 
-  // Init the Express application
   const app = express();
-
-  // Set the network port
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT || 8080; // default port to listen
   
-  // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //CORS Should be restricted
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
-  /**************************************************************************** */
+  app.use('/api/v0/', IndexRouter)
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
+  // Root URI call
   app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.send( "/api/v0/" );
   } );
   
 
